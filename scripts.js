e01cd6ac78344813c2eaecb24346448ff1a5f0ea
Bogusdrac <bogusdracula7@gmail.com>
2024-10-25 12:17:00 +0200

    scenario 3 , console error when dividing by zero ; and scenario 4, crashing the program when either input in NaN

--- scripts.js
+++ scripts.js
@@ -8,8 +8,22 @@ form.addEventListener("submit", (event) => {
 
   // validation for when values are missing
   if (!dividend || !divider) {
-    result.innerHTML = "Division can't be done. Both inputs are required."
+    result.textContent = "Division can't be done. Both inputs are required."
     return;
+  }
+
+  // An invalid division should log an error in the console
+  if (Number(divider) === 0) {
+    result.textContent = "Division not performed. Invalid number provided. Try again";
+    console.error(new Error("Attempted division by zero"));
+    return;
+  }
+
+  // Providing anything that is not a number should crash the program
+  if (isNaN(dividend) || isNaN(divider)) {
+    console.error("Error: Non-numeric input");
+    document.body.innerHTML = "<h1>Something critical went wrong. Please reload the page.</h1>";
+    throw new Error("Non-numeric imput provided.")
   }
 
   // This will divide and round out the result if it's a decimal
